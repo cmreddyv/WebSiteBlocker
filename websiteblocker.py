c33a3d3d9b59acd1a7d1e49ac659265334ffe12c
Chandra Mohan Reddy Veeramreddy <33657970+cmreddyv@users.noreply.github.com>
2020-05-05 13:41:34 +0530

    updated with comments

--- websiteblocker.py
+++ websiteblocker.py
@@ -2,20 +2,22 @@ import time
 
 from datetime import datetime as dt
 
-
+#hosts file path
 hosts_path = "/etc/hosts"
+
+
 
 redirect = "127.0.0.1"
 
-
+#list of websites which you want to block
 website_list =["www.facebook.com","facebook.com","instagram.com","www.instagram.com"]
 
 while True:
+    #time which you want to block the websites, here it is from 9 am to 10 PM daily
     if dt(dt.now().year,dt.now().month,dt.now().day,9) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,22):
         print("Working hours")
         with open(hosts_path, 'r+') as file:
             content = file.read()
-            #print(content)
             for website in website_list:
                 if website in content:
                     pass
@@ -32,4 +34,5 @@ while True:
 
 
         print("Fun hours:")
+    #time to check the script
     time.sleep(60)
